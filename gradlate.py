6b902896763e33725e44e979c0e0d260cf74c545
Arseniy Simonov <ars@exasol.com>
2018-09-20 16:21:19 +0200

    chunkify before aligning

--- gradlate.py
+++ gradlate.py
@@ -4,8 +4,11 @@ import getopt
 from collections import Counter as Counter
 import re
 from nltk.stem.snowball import SnowballStemmer as SS
+from nltk.translate.gale_church import align_blocks
 
 word_separator = re.compile('[ \n\'".,/:;!?()]+')
+sentence_separator = re.compile('[".!?()]+')
+empty_sentence = re.compile('^\s*$')
 
 def help_exit():
     print('test.py -f <language>:<from_translation> -t <language>:<to_translation> -o <outputfile>')
@@ -13,17 +16,33 @@ def help_exit():
 
 def normalize(w): return w.lower()
 
-def count_freqs(text, stemmer):
-    ctr = Counter([stemmer.stem(w) for w in word_separator.split(text)])
-    return [t for t in ctr.most_common() if t[1] > 5 and len(t[0]) > 1]
+class Text:
+    def __init__(self, filename):
+        with open(filename, 'r') as f:
+            self.raw_text = f.read()
+        self.sentences = [s for s in sentence_separator.split(self.raw_text) if not empty_sentence.match(s)]
+        self.words = word_separator.split(self.raw_text)
+        self.sentence_lengths = [len(s) for s in self.sentences]
 
-def gradlate(ft, fs, tt, ts, out):
-    fs.maxCacheSize = 1000000
-    ts.maxCacheSize = 1000000
-    f_freqs = count_freqs(ft, fs)
-    t_freqs = count_freqs(tt, ts)
-    print(f_freqs[0:200])
-    print(t_freqs[0:200])
+def chunk_list(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+def gradlate(ft, tt, out):
+    print('{} {}'.format(len(ft.sentence_lengths), len(tt.sentence_lengths)))
+#        print('{} {}'.format(len(s), s))
+
+    parts = 100
+    ft1 = chunk_list(ft.sentence_lengths, parts)[0]
+    tt1 = chunk_list(tt.sentence_lengths, parts)[0]
+    print(align_blocks(ft1, tt1))
 
 if __name__ == '__main__':
     try:
@@ -46,11 +65,9 @@ if __name__ == '__main__':
     if not all([from_path, to_path]):
         help_exit()
 
-    with open(from_path, 'r') as from_file:
-        with open(to_path, 'r') as to_file:
-            if out_path:
-                with open(out_path, 'w') as out_file:
-                    gradlate(from_file.read(), SS(from_lang), to_file.read(), SS(to_lang), out_file)
-            else:
-                gradlate(from_file.read(), SS(from_lang), to_file.read(), SS(to_lang), sys.stdout)
+    if out_path:
+        with open(out_path, 'w') as out_file:
+            gradlate(Text(from_path), Text(to_path), out_file)
+    else:
+        gradlate(Text(from_path), Text(to_path), sys.stdout)
 
